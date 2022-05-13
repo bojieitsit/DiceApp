e7e39a86b7d634091e1e3104ea6752f9657b4e88
Andrey Bogoslovsky <bojieitsit@icloud.com>
2022-05-13 19:13:33 +0400

    Simple Dice App

--- Dice App/ViewController.swift
+++ Dice App/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceView1: UIImageView!
+    @IBOutlet weak var diceView2: UIImageView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        let rundomNumber1 = Int.random(in: 0...5)
+        let rundomNumber2 = Int.random(in: 0...5)
+        
+        diceView1.image = [UIImage(named: "1"),UIImage(named: "2"),UIImage(named: "3"),UIImage(named: "4"),UIImage(named: "5"),UIImage(named: "6"),][rundomNumber1]
+        diceView2.image = [UIImage(named: "1"),UIImage(named: "2"),UIImage(named: "3"),UIImage(named: "4"),UIImage(named: "5"),UIImage(named: "6"),][rundomNumber2]
+    }
+    
 
 }
 
